6ec0fbe3d16b8b4a8f3ac8d97903de210e531e76
Tom <thomas.spencer@codurance.com>
2021-08-02 17:21:41 +0100

    add empty exception

--- src/main/java/com/codurance/List.java
+++ src/main/java/com/codurance/List.java
@@ -1,7 +1,11 @@
 package com.codurance;
 
-public class List {
+public class List<T> {
   public int size() {
     return 0;
+  }
+
+  public T get(int i) {
+    throw new IndexOutOfBoundsException();
   }
 }
--- src/test/java/com/codurance/ListShould.java
+++ src/test/java/com/codurance/ListShould.java
@@ -1,8 +1,10 @@
 package com.codurance;
 
+import org.assertj.core.api.IndexedObjectEnumerableAssert;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 public class ListShould {
@@ -12,5 +14,14 @@ public class ListShould {
     int size = list.size();
 
     assertThat(size).isEqualTo(0);
+  }
+
+  @Test
+  void get_element_fromEmptyArray_throwsAnException() {
+    List list = new List();
+
+    assertThrows(IndexOutOfBoundsException.class, () -> {
+      list.get(0);
+    });
   }
 }
